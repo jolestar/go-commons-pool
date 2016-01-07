4f2a92597bb0818d25ff8aa8259c45e535bd642a
jolestar <jolestar@gmail.com>
2016-01-07 20:35:44 +0800

    add more factory test.

--- factory_test.go
+++ factory_test.go
@@ -3,14 +3,17 @@ package pool
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
-	"math/rand"
 	"testing"
 )
 
-func TestDefaultPooledObjectFactory(t *testing.T) {
+type TestFactoryObject struct {
+	status string
+}
+
+func TestDefaultPooledObjectFactorySimple(t *testing.T) {
 	factory := NewPooledObjectFactorySimple(
 		func() (interface{}, error) {
-			return &TestObject{Num: rand.Int()}, nil
+			return &TestFactoryObject{status: "make"}, nil
 		})
 
 	assert.NotNil(t, factory)
@@ -21,4 +24,42 @@ func TestDefaultPooledObjectFactory(t *testing.T) {
 	assert.Nil(t, factory.PassivateObject(o))
 	assert.True(t, factory.ValidateObject(o))
 	assert.Nil(t, factory.DestroyObject(o))
+}
+
+func TestDefaultPooledObjectFactory(t *testing.T) {
+	factory := NewPooledObjectFactory(
+		func() (interface{}, error) {
+			return &TestFactoryObject{status: "make"}, nil
+		},
+		func(object *PooledObject) error {
+			object.Object.(*TestFactoryObject).status = "destory"
+			return nil
+		},
+		func(object *PooledObject) bool {
+			object.Object.(*TestFactoryObject).status = "validate"
+			return true
+		},
+		func(object *PooledObject) error {
+			object.Object.(*TestFactoryObject).status = "activate"
+			return nil
+		},
+		func(object *PooledObject) error {
+			object.Object.(*TestFactoryObject).status = "passivate"
+			return nil
+		})
+
+	assert.NotNil(t, factory)
+	o, _ := factory.MakeObject()
+	fmt.Println("object:", o.Object)
+	assert.NotNil(t, o)
+	obj := o.Object.(*TestFactoryObject)
+	assert.Equal(t, "make", obj.status)
+	assert.Nil(t, factory.ActivateObject(o))
+	assert.Equal(t, "activate", obj.status)
+	assert.Nil(t, factory.PassivateObject(o))
+	assert.Equal(t, "passivate", obj.status)
+	assert.True(t, factory.ValidateObject(o))
+	assert.Equal(t, "validate", obj.status)
+	assert.Nil(t, factory.DestroyObject(o))
+	assert.Equal(t, "destory", obj.status)
 }
